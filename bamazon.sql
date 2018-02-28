2583b01cbaa5b3d883085ea5bb84c714f17659c1
Nick Volkert <nick@nickvolkert.com>
2018-02-27 23:09:26 -0600

    progress save

---
+++ bamazon.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  dept_name VARCHAR(100) NOT NULL,
+  category VARCHAR(45) NOT NULL,
+  price DECIMAL(10, 4) NULL,
+  stock_qty DECIMAL(10, 4) NULL,
+  PRIMARY KEY (id)
+);
