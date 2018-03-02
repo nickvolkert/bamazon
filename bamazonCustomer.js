4ad3967f34fe8c5740fa5afdd731d2895f2bc4cb
Nick Volkert <nick@nickvolkert.com>
2018-03-01 23:08:17 -0600

    saving progress

---
+++ bamazonCustomer.js
@@ -0,0 +1,161 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazonDB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+function start() {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the ID of the item you would like to buy (1-10)?"
+      },
+      {
+        name: "startingBid",
+        type: "input",
+        message: "How many units of" + inventory + "would you like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ]) .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid.toUpperCase() === "POST") {
+        postAuction();
+      }
+      else {
+        bidAuction();
+      }
+    });
+}
+
+function createProduct() {
+  console.log("Inserting a new product...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      flavor: "Rocky Road",
+      price: 3.0,
+      quantity: 50
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function updateProduct() {
+  console.log("Updating all Rocky Road quantities...\n");
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        quantity: 100
+      },
+      {
+        flavor: "Rocky Road"
+      }
+    ],
+    function(err, res) {
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function deleteProduct() {
+  console.log("Deleting all strawberry icecream...\n");
+  connection.query(
+    "DELETE FROM products WHERE ?",
+    {
+      flavor: "strawberry"
+    },
+    function(err, res) {
+      console.log(res.affectedRows + " products deleted!\n");
+      // Call readProducts AFTER the DELETE completes
+      readProducts();
+    }
+  );
+}
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+// function to handle posting new items up for auction
+function postAuction() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the ID of the item you would like to buy (1-10)?"
+      },
+      {
+        name: "startingBid",
+        type: "input",
+        message: "How many units of" + inventory + "would you like to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+          item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid,
+          highest_bid: answer.startingBid
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        }
+      );
+    });
+}
