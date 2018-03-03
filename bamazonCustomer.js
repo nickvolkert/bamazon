4b8a429fe8bba7b285ab008294e2bcc80cecd04b
Nick Volkert <nick@nickvolkert.com>
2018-03-03 00:43:55 -0600

    saving progress

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,23 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var argumentTwo = process.argv[2];
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "",
   database: "bamazonDB"
 });
 
-// connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
   start();
 });
 
+// function to handle posting new items up for auction
 function start() {
+  // prompt for info about the item being put up for auction
   inquirer
     .prompt([
       {
@@ -29,7 +26,7 @@ function start() {
         message: "What is the ID of the item you would like to buy (1-10)?"
       },
       {
-        name: "startingBid",
+        name: "purchaseUnits",
         type: "input",
         message: "How many units of" + inventory + "would you like to buy?",
         validate: function(value) {
@@ -39,39 +36,28 @@ function start() {
           return false;
         }
       }
-    ]) .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid.toUpperCase() === "POST") {
-        postAuction();
-      }
-      else {
-        bidAuction();
-      }
+    ])
+    .then(function(answer) {
+      // when finished prompting, insert a new item into the db with that info
+      var userItemRequest = answers.product_name;
+      var productStock = answers.stock_qty;
+      connection.query("SELCT product_name WHERE ?",
+        {
+          item_name: answer.product_name
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("Your auction was created successfully!");
+          // re-prompt the user for if they want to bid or post
+          start();
+        }
+      );
     });
 }
 
-function createProduct() {
-  console.log("Inserting a new product...\n");
-  var query = connection.query(
-    "INSERT INTO products SET ?",
-    {
-      flavor: "Rocky Road",
-      price: 3.0,
-      quantity: 50
-    },
-    function(err, res) {
-      console.log(res.affectedRows + " product inserted!\n");
-      // Call updateProduct AFTER the INSERT completes
-      updateProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
 
 function updateProduct() {
-  console.log("Updating all Rocky Road quantities...\n");
+  console.log("Updating all product quantity...\n");
   var query = connection.query(
     "UPDATE products SET ? WHERE ?",
     [
@@ -79,7 +65,7 @@ function updateProduct() {
         quantity: 100
       },
       {
-        flavor: "Rocky Road"
+        product_name: 
       }
     ],
     function(err, res) {
@@ -93,21 +79,6 @@ function updateProduct() {
   console.log(query.sql);
 }
 
-function deleteProduct() {
-  console.log("Deleting all strawberry icecream...\n");
-  connection.query(
-    "DELETE FROM products WHERE ?",
-    {
-      flavor: "strawberry"
-    },
-    function(err, res) {
-      console.log(res.affectedRows + " products deleted!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
-}
-
 function readProducts() {
   console.log("Selecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
@@ -116,46 +87,4 @@ function readProducts() {
     console.log(res);
     connection.end();
   });
-}
-
-// function to handle posting new items up for auction
-function postAuction() {
-  // prompt for info about the item being put up for auction
-  inquirer
-    .prompt([
-      {
-        name: "item",
-        type: "input",
-        message: "What is the ID of the item you would like to buy (1-10)?"
-      },
-      {
-        name: "startingBid",
-        type: "input",
-        message: "How many units of" + inventory + "would you like to buy?",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      // when finished prompting, insert a new item into the db with that info
-      connection.query(
-        "INSERT INTO auctions SET ?",
-        {
-          item_name: answer.item,
-          category: answer.category,
-          starting_bid: answer.startingBid,
-          highest_bid: answer.startingBid
-        },
-        function(err) {
-          if (err) throw err;
-          console.log("Your auction was created successfully!");
-          // re-prompt the user for if they want to bid or post
-          start();
-        }
-      );
-    });
 }
